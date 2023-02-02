0c0941cdc334573abcdbbe5d29356c914a46646a
Grigory <kononoff88@gmail.com>
2023-02-02 21:36:29 +0300

    1

--- MiQueue.java
+++ MiQueue.java
@@ -5,26 +5,24 @@ import java.util.LinkedList;
 // dequeue() - возвращает первый элемент из очереди и удаляет его, 
 // first() - возвращает первый элемент из очереди, не удаляя.
 
-
 public class MiQueue {
 	private LinkedList<String> list = new LinkedList<>();
-	
-	
-	MiQueue (int capacity){
+
+	MiQueue(int capacity) {
 		LinkedList<String> list = new LinkedList<>();
 	}
 
-	LinkedList<String> enqueue(String e){
+	LinkedList<String> enqueue(String e) {
 		list.addLast(e);
 		return list;
 	}
 
-	String dequeue(){
+	String dequeue() {
 		return list.remove(0);
 
 	}
 
-	String first(){
+	String first() {
 		return list.get(0);
 	}
 }
